2f06e4420a4e74156fd94d56f0d52ba9c425e532
sofianeallouche <sofianeallouche@yahoo.com>
2026-03-04 23:56:38 +0100

    création app profiles avec models, views, urls, templates et migration des données

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Profile
+
+
+def index(request):
+    """Vue affichant la liste de tous les profils."""
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    """Vue affichant le détail d'un profil utilisateur."""
+    profile = Profile.objects.get(user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
