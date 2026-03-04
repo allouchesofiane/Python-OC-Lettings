922b231594ff940f62d62baead682f4faecfc8be
sofianeallouche <sofianeallouche@yahoo.com>
2026-03-04 23:17:06 +0100

    refactor: création app lettings avec models, views, urls, admin

---
+++ lettings/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Letting
+
+
+def index(request):
+    """Vue affichant la liste de toutes les locations."""
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    """Vue affichant le détail d'une location."""
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'lettings/letting.html', context)
