c453e31ef129b7030cf49617c75e105f57b21ebf
sofianeallouche <sofianeallouche@yahoo.com>
2026-03-19 22:04:53 +0100

    Ajout du domaine Render dans ALLOWED_HOSTS

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'python-oc-lettings-l2ou.onrender.com']
 
 # Sentry configuration
 sentry_sdk.init(
